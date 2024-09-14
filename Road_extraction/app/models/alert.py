993119f21e33376eb76a694669a3f1953a301134
Dushyanth Yalamanchili <167439018+dushu-108@users.noreply.github.com>
2024-09-14 16:29:43 +0530

    Update alert.py

---
+++ Road_extraction/app/models/alert.py
@@ -0,0 +1,5 @@
+class Alert:
+    def __init__(self, area_id, change_detected, message):
+        self.area_id = area_id
+        self.change_detected = change_detected
+        self.message = message
